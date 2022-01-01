35b1657527892eb2d6593471f604eff279878bca
David Kurniawan <96937707+Rizaldntro@users.noreply.github.com>
2022-01-01 19:43:54 +0700

    commit uts

---
+++ lib/main.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "REJI AINUROHIM ",
+            style: new TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+        ),
+        //backgroundColor: Colors.blue[100],
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.grey),
+                    child: const Center(
+                      child: Text(
+                        "grey",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.blueGrey),
+                    child: const Center(
+                      child: Text(
+                        "blueGrey",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.greenAccent),
+                    child: const Center(
+                      child: Text(
+                        "greenAccent",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.lightGreenAccent),
+                    child: const Center(
+                      child: Text(
+                        "light Green",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.lightBlue),
+                    child: const Center(
+                      child: Text(
+                        "lightBlue",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.lightGreenAccent),
+                    child: const Center(
+                      child: Text(
+                        "lightGreen",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.pink),
+                    child: const Center(
+                      child: Text(
+                        "pink",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.orangeAccent),
+                    child: const Center(
+                      child: Text(
+                        "black",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.black),
+                    child: const Center(
+                      child: Text(
+                        "black",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
